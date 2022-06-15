8fa73f502e1c8b5a1c778b86a4ca38035a351fe0
chriskid <khoolemin.04@gmail.com>
2022-06-15 17:04:57 +0800

    doc: experimenting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,44 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-
+import React from "react"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 style={{fontSize: 60, fontFamily: 'Bradley Hand'}}>Honeycomb</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+     <div className="App">
+     <header className="App-header">
+     <h1 style={{fontSize: 80, fontFamily: 'Bradley Hand'}}>Honeycomb</h1>
+       <p>Hello Vite + React!</p>
+       <p>
+         <button type="button" onClick={() => setCount((count) => count + 1)}>
+           count is: {count}
+         </button>
+       </p>
+       <p>
+         Edit <code>App.jsx</code> and save to test HMR updates.
+       </p>
+       <p>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+         {' | '}
+         <a
+           className="App-link"
+           href="https://vitejs.dev/guide/features.html"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Vite Docs
+         </a>
+       </p>
+     </header>
+   </div>
+   
   )
 }
-
 export default App
