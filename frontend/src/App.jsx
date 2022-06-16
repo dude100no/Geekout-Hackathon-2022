696f9eefc54b44369b5b69a41d8f4d69e89ac044
NicholasJohansan <nicholasjohansan@gmail.com>
2022-06-16 10:15:36 +0800

    Separate styling into styles folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
+import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
-
+import "../styles/home.css";
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{ fontSize: 80, fontFamily: "Bradley Hand" }}>Honeycomb</h1>
-        <p style={{ fontSize: 30, fontFamily: "monospace" }}>
-          Welcome to Honeycomb, a web app that's here to help.
-        </p>
-      </header>
-      <>
-  <style type="text/css">
-    {`
-    .btn-flat {
-      background-color: orange;
-      color: white;
-    }
-
-    .btn-xxl {
-      padding: 1rem 1.5rem;
-      font-size: 1.5rem;
-    }
-    `}
-  </style>
-
-  <Button variant="flat" size="xl" onClick={() => navigate("/signup")}>Get Started</Button>
-  <Button variant="flat" size="xl" onClick={() => navigate("/profile")}>Sign In</Button>
-</>
-      
+    <div id="home-page">
+      <h1>Honeycomb</h1>
+      <p>Welcome to Honeycomb, a web app that's here to help.</p>
+      <div className="buttons">
+        <Button variant="flat" size="xl" onClick={() => navigate("/signup")}>
+          Get Started
+        </Button>
+        <Button variant="flat" size="xl" onClick={() => navigate("/profile")}>
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 }
