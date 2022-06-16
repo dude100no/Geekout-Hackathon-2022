d13f6d0463d869098e81a5f7c7becd5b8495b390
chriskid <khoolemin.04@gmail.com>
2022-06-16 10:10:46 +0800

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 //change background color
+import { useNavigate } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
 function SignUp() {
   return (
    <div className="Sign">
       <header className="Signup-header">
-        <h1 style={{ fontSize: 80, fontFamily: "monospace" }}>Sign Up</h1>
-        <p style={{ fontSize: 30, fontFamily: "monospace" }}>
-          Choose your preferred log in method.
+        <h1 style={{ fontSize: 50, fontFamily: "monospace" }}>Sign Up</h1>
+        <p style={{ fontSize: 30, fontFamily: "monospace" }}>Choose your preferred log in method.
         </p>
       </header>
   </div>
