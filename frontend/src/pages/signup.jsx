4acb946d8cc7b4c7e444fc3a521be8c287263511
chriskid <khoolemin.04@gmail.com>
2022-06-16 10:06:52 +0800

    signup page

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+//change background color
 function SignUp() {
-  return <div>sign up page</div>;
+  return (
+   <div className="Sign">
+      <header className="Signup-header">
+        <h1 style={{ fontSize: 80, fontFamily: "monospace" }}>Sign Up</h1>
+        <p style={{ fontSize: 30, fontFamily: "monospace" }}>
+          Choose your preferred log in method.
+        </p>
+      </header>
+  </div>
+  )
 }
 export default SignUp;
