984a9324f2dedf47c4ab326c07808ec9bff49a6e
chriskid <khoolemin.04@gmail.com>
2022-06-16 16:56:16 +0800

    Update other.jsx

--- frontend/src/pages/other.jsx
+++ frontend/src/pages/other.jsx
@@ -4,16 +4,18 @@ import "../styles/other.css";
 import { Link } from "react-router-dom";
 
 function Other() {
-  return <div>
+  return <header id="other-page">
+  <div>
     <h1>
         Other Resources
     </h1>
-    <p>Here are other resources that will help you in your journey :D
+    <p>Here are other resources that will help you in your journey.
     </p>
-    <h2> <Link to ="https://www.myskillsfuture.gov.sg"></Link>SkillsFuture</h2>
+    <h2> <Link to="https://www.myskillsfuture.gov.sg/content/portal/en/index.html">SkillsFuture</Link></h2>
     <p>
     <Link to="https://www.myskillsfuture.gov.sg/content/portal/en/assessment/landing.html">Self-Assesement Tool</Link>
     </p>
-  </div>;
+  </div>
+  </header>
 }
 export default Other;
